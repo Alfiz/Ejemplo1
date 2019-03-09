874388a7c5ae61621973e3ce93e132957ddc102f
Alfredo Jimenez <Alfredo_Diplomado@macbook-10.local>
2019-03-08 19:52:57 -0600

    Initial Commit

---
+++ Clas11Ej2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clas11Ej2
+//
+//  Created by Alfredo Jimenez on 3/8/19.
+//  Copyright © 2019 Alfredo Jimenez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
